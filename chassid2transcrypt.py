e146be154000e5075f08c001af0b9cce9b5a5c91
jesseos <31975328+jesseos@users.noreply.github.com>
2023-05-31 23:35:47 -0400

    Add files via upload

---
+++ chassid2transcrypt.py
@@ -0,0 +1,68 @@
+gmagnitude = 0.003
+
+class eye():
+    def __init__(self, x, y, magnitude):
+        self.x = x
+        self.originX = x
+        self.y = y
+        self.originY = y
+        self.originMagnitude = magnitude 
+        self.magnitude = magnitude
+        self.distX = 0
+        self.distY = 0
+        self.radiusX = 12
+        self.radiusY = 5
+        
+    def update(self):
+        self.distX = mouseX-self.x
+        self.distY = mouseY-self.y
+                
+        if (abs(self.x-self.originX)) <= self.radiusX:
+            self.magnitude = self.originMagnitude
+            self.x = self.x + (self.distX * self.magnitude)
+        else:
+            print(abs(((self.x-self.originX))-self.radiusX))
+            if self.x > self.originX:
+                self.x = self.x - (abs(((self.x-self.originX))-self.radiusX))
+            else:
+                self.x = self.x + (abs(((self.x-self.originX))+self.radiusX))
+
+            #self.x = self.x (
+            self.magnitude = 0
+        if (abs(self.y-self.originY)) <= self.radiusY:
+            self.magnitude = self.originMagnitude
+            self.y = self.y + (self.distY * self.magnitude)
+        else:
+            if self.y > self.originY:
+                self.y = self.y - (abs(((self.y-self.originY))-self.radiusY))
+            else:
+                self.y = self.y + (abs(((self.y-self.originY))+self.radiusY))
+
+
+
+    def display(self):
+        fill(0)
+        ellipse(self.x, self.y, 15, 15)
+        
+img = []
+
+def setup():
+    size(500, 586)
+    global img
+    img = loadImage("logofunctional.png")
+    noStroke()
+    
+eyel = eye(185, 250, gmagnitude)
+eyer = eye(313, 250, gmagnitude)
+
+def draw():
+    background(255)
+    rect(0, 0, 500, 586)
+    fill(0)
+    eyel.update()
+    eyer.update()
+    eyel.display()
+    eyer.display()
+    fill(255)
+    image(img, 0, 0)
+    
